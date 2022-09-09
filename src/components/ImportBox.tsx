8d3f22a04799c651a4044598c6955bcddf739ca7
Edward Tanguay (windows) <edward@tanguay.info>
2022-09-10 00:34:42 +0200

    init commit

---
+++ src/components/ImportBox.tsx
@@ -0,0 +1,12 @@
+import { useStore } from '../store';
+import { FaSpinner } from 'react-icons/fa';
+
+export const ImportBox = () => {
+	const store = useStore((state) => state);
+
+	return (
+		<div className="importBox">
+			nnn
+		</div>
+	);
+};

487d8caf04c9552142c6a005dae1ea3485502268
Edward Tanguay (windows) <edward@tanguay.info>
2022-09-10 02:03:52 +0200

    test parsing

--- src/components/ImportBox.tsx
+++ src/components/ImportBox.tsx
@@ -4,18 +4,34 @@ import { FaSpinner } from 'react-icons/fa';
 export const ImportBox = () => {
 	const store = useStore((state) => state);
 
-	const handleInputTextChange = (e:any) => {
-		const value = e.target.value;
-		store.setInputText(value);
-		store.setOutputText(value + ' ADDED');
-	}
+	const handleInputTextChange = (e: any) => {
+		const text = e.target.value;
+		if (text.trim() !== '') {
+			const _lines = text.split('\n');
+			const lines: string[] = [];
+			_lines.forEach((line: string) => {
+				lines.push(line + 'nnn');
+			});
+			const parsedText = lines.join('\n');
+
+			store.setInputText(text);
+			store.setOutputText(parsedText);
+		} else {
+			store.setInputText('');
+			store.setOutputText('');
+		}
+	};
 
 	return (
 		<div className="importBox">
 			<h2>Learn card text to parse:</h2>
-			<textarea value={store.inputText} onChange={handleInputTextChange}></textarea>	
+			<textarea
+				value={store.inputText}
+				spellCheck="false"
+				onChange={handleInputTextChange}
+			></textarea>
 			<h2>Learn cards JSON to paste:</h2>
-			<textarea value={store.outputText} readOnly></textarea>	
+			<textarea value={store.outputText} readOnly></textarea>
 		</div>
 	);
 };
